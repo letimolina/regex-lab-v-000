3791646cb89292a2a9e58fd4c8df42a7ebcd0b87
Leticia Molina <letimolina@gmail.com>
2018-05-18 01:40:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,9 +18,13 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/\A[A-Z].+[.,?!]\z/)
+    true
+  else
+    false
 
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/d\)
 end
