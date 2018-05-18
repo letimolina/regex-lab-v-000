1c37ebcf58b0f9833269b2171add86e45746aab8
Leticia Molina <letimolina@gmail.com>
2018-05-18 01:45:31 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,23 +1,34 @@
 def starts_with_a_vowel?(word)
-if !word.scan(/\b[aeiou][a-z]*\b/)
- false
-else 
+if word.match(/\b[aeiouAEIOU][a-z]*\b/)
  true
+else
+ false
 end
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
+text.scan(/un\w+ing/)
 
 end
 
-def words_five_letters_long(text)
 
+def words_five_letters_long(text)
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z].+[.,?!]\z/)
+    true
+  else
+    false
+end
 end
 
 def valid_phone_number?(phone)
-
+if phone.scan(/\d/).length == 10
+  true
+else
+  false
+end
 end
